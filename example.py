5f5007387b07d348420286eb631a7c3523286b51
Fsoky <cyberuest0x12@gmail.com>
2022-03-02 15:03:47 +0600

    change: `friends_list` -> `friends`

--- example.py
+++ example.py
@@ -5,11 +5,11 @@ api = Steam("API_KEY")
 
 
 async def handler():
-	friends = await api.user.friends(76561198982570889)
+    friends = await api.users.friends(76561198982570889)
 
-	for friend in friends.friends_list:
-		usr = await api.user.get(friend.steam_id)
-		print(usr.players[0].name)
+    for friend in friends.friends:
+        usr = await api.users.get(friend.steam_id)
+        print(usr.players[0].name)
 
 
 asyncio.get_event_loop().run_until_complete(handler())
